b5e4e01be79e24cac658c4e4b03edad2acca7d06
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2025-01-15 08:45:48 +0000

    Change project name, change app name, modify Procfile

--- my_project/urls.py
+++ my_project/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from foody.views import my_foody
+from django.urls import path, include
+from foody.views import hello_cuisine
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('foody/', my_foody, name='foody')
+    path('foody/', hello_cuisine, name='foody')
 ]
