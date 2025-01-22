c6673a715293d2d080574860d615aed6fefd11ad
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2025-01-22 23:19:05 +0000

    Create html file

--- foody/views.py
+++ foody/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello foodies, Welcome to Comfort Foods Cuisine Germany!")
+    return render(request, "foody/index.html")
