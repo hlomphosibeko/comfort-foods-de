bf56c320f5aa8555949a97ca10b37dba12503e35
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-02-03 13:47:08 +0000

    Create env., set the Database url inside it

--- developer/settings.py
+++ developer/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
 
 # Application definition
