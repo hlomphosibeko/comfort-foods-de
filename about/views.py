1bb84b399081f9e922a3f8a11a90d020a31a197b
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-02-04 21:19:07 +0000

    Amend about.html file

--- about/views.py
+++ about/views.py
@@ -7,19 +7,19 @@ def about_us(request):
     """
     Renders the About page.
     """
-    if request.method == "POST":
-        collaborate_form = CollaborateForm(data=request.POST)
-        if collaborate_form.is_valid():
-            collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS)
+    # if request.method == "POST":
+    #     collaborate_form = CollaborateForm(data=request.POST)
+    #     if collaborate_form.is_valid():
+    #         collaborate_form.save()
+    #         messages.add_message(request, messages.SUCCESS)
 
-    about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
+    # about = About.objects.all().order_by('-updated_on').first()
+    # collaborate_form = CollaborateForm()
 
     return render(
         request, 
         "about/about.html",
-        {"about": about,
-        "collaborate_form":collaborate_form,
+        {"about": about_us,
+        # "collaborate_form":collaborate_form,
         },
         )
