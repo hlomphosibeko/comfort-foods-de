b8422bac1c85c5236fb8f46d27c356277f662e3d
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-02-05 09:55:47 +0000

    Change index.html to home.html, update the views and urls

--- foody/views.py
+++ foody/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return render(request, "foody/index.html")
+def home(request):
+    return render(request, "foody/home.html")
