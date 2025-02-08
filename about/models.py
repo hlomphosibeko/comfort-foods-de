2b9335dbc6749062b834a989572a83e72f713b05
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-02-08 18:14:09 +0000

    Add placeholder image to the about page

--- about/models.py
+++ about/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class About(models.Model):
@@ -9,7 +8,6 @@ class About(models.Model):
     profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-
 
     def __str__(self):
         return self.title
--- about/views.py
+++ about/views.py
@@ -7,20 +7,20 @@ def about_us(request):
     """
     Renders the About page.
     """
-    about = About.objects.all().order_by('-updated_on').first()
     # if request.method == "POST":
     #     collaborate_form = CollaborateForm(data=request.POST)
     #     if collaborate_form.is_valid():
     #         collaborate_form.save()
     #         messages.add_message(request, messages.SUCCESS)
 
-    # about = About.objects.all().order_by('-updated_on').first()
+    about = About.objects.all().order_by('-updated_on').first()
+   
     # collaborate_form = CollaborateForm()
 
     return render(
         request, 
         "about/about.html",
-        {"about": about_us,
+        {"about": about,
         # "collaborate_form":collaborate_form,
         },
         )
