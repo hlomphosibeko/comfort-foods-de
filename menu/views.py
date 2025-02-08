be3b0f2da0e104f7faefef6e1f50a99a356963c2
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-02-08 22:40:34 +0000

    Adding a loop for the menu

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Menu
 
 # Create your views here.
 def menu(request):
-    return render(request, "menu/menu.html")
+    my_menu = Menu.objects.all()
+    return render(request, "menu/menu.html",
+                  {'my_menu': my_menu})
