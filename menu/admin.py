eae35b0871c934bbc70494f454ffb7c27e6303d3
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-02-10 21:21:23 +0000

    Update Menu models with a Customer class, Update admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, Customer
 
 # Register your models here.
 admin.site.register(Menu)
+admin.site.register(Customer)
 
 
--- menu/models.py
+++ menu/models.py
@@ -5,15 +5,24 @@ from cloudinary.models import CloudinaryField
 class Menu(models.Model):
     meal_name = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    category = models.CharField(max_length=250)
     description = models.CharField(
         max_length=200, default='', blank=True, null=True
     )
     price = models.PositiveIntegerField()
-    images = CloudinaryField('image')
+    images = CloudinaryField('image', blank=True)
+
+    def __str__(self):
+        return self.meal_name
 
 
+class Customer(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField()
 
-
+    def __str__(self):
+        return self.first_name
  
 
 
