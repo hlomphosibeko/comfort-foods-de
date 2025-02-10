df2c31d56da6ffdc11f8a1b32943eead0a487718
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-02-10 19:37:32 +0000

    Change Menu models, delete redundant code

--- menu/models.py
+++ menu/models.py
@@ -1,38 +1,19 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-
 # Create your models here.
-class Customer(models.Model):
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.first_name
-
-
 class Menu(models.Model):
-    meal_name = models.CharField(max_length=200)
+    meal_name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
     description = models.CharField(
         max_length=200, default='', blank=True, null=True
     )
-    price = models.IntegerField(default=0)
-    images = CloudinaryField('image', default='placeholder')  
+    price = models.PositiveIntegerField()
+    images = CloudinaryField('image')
 
-    def __str__(self):
-        return self.meal_name 
 
-class Orders(models.Model):
-    meal = models.ForeignKey(
-        Menu, on_delete=models.CASCADE
-    )
-    customer = models.ForeignKey(
-        Customer, on_delete=models.CASCADE
-    )
-    status = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.status
+
+ 
 
 
--- menu/views.py
+++ menu/views.py
@@ -4,5 +4,5 @@ from .models import Menu
 # Create your views here.
 def menu(request):
     my_menu = Menu.objects.all()
-    return render(request, "menu/menu.html",
-                  {'my_menu': my_menu})
+    return render(request, "menu/menu.html",{
+        'my_menu': my_menu})
