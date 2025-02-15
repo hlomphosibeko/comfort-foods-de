31b443e26d50bb1fba63da70fd437e28d498cdff
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-02-15 20:39:04 +0000

    Add images for the README.md file, delete redundant code

---
+++ static/js/myscript.js
@@ -0,0 +1,37 @@
+/**
+ * When the DOMContent is loaded, it will trigger a function.
+ * An eventlistener will listen for the radio buttons to be clicked.
+ */
+
+// const editButton = document.getElementsByClassName("btn-edit")
+//const commentText = document.getElementById("id_body").value
+const commentForm = document.getElementById("menuForm")
+// const submitButton = document.getElementsByClassName("submitButton")
+
+//console.log(commentText.innerHTML)
+console.log(commentForm)
+//const mytext = document.getElemetById("id_text").innerhtml
+//console.log(mytext)
+
+// for (let myval of commentText)
+
+// // {
+//     console.log(myval) 
+// }
+
+// for (let button of editButton) {
+//     button.addEventListener("click", (e) => {
+//         let menuId = e.target.getAttribute("menu_id");
+//         let feedbackContent = document.getElementById(`text${menuId}`).innerText;
+//         commentText.value = feedbackContent;
+//         submitButton.innerText = "Update";
+//         commentForm.setAttribute("action", `edit_feedback/${menuId}`);
+//     });
+// }
+
+//const submitLink = document.getElementById();
+
+//for (let button of submitLink);
+
+
+// const carousel = new bootstrap.Carousel('#myCarousel')
