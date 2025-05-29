ed93071134f3125600117967ed68c6eaaf12648a
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-05-29 09:39:22 +0000

    create generic menu view

--- developer/urls.py
+++ developer/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from menu.views import my_menu
+from django.urls import path, include
+
 # from django.conf.urls.static import static
 # from django.conf import settings
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("menu/", my_menu, name="menu")
+    path("menu/", include("menu.urls"), name="menu-urls"),
     # path('', include("foody.urls"), name="foody-urls"),
     # path("about/", include("about.urls"), name="about-urls"),
     # path("accounts/", include("allauth.urls")),
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Menu
+
 
 # Create your views here.
-def my_menu(request):
-    return HttpResponse("Hello, menu!")
+class MenuList(generic.ListView):
+    queryset = Menu.objects.all()
+    template_name = "menu_list.html"
