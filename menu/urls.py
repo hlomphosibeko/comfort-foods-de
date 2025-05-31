72c8847c3ebebce50cb7c98feabb4073a99d62e2
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-05-31 09:17:44 +0000

    Add menu_detail function-based view, add to the url list, create menu_detail.html file

--- menu/urls.py
+++ menu/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='menu')
+    path('', views.MenuList.as_view(), name='menu'),
+    path('<slug:slug>/', views.menu_detail, name='menu_detail'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Menu
 
@@ -8,3 +8,27 @@ class MenuList(generic.ListView):
     queryset = Menu.objects.filter(status=0)
     template_name = "menu/index.html"
     paginate_by = 6
+
+
+def menu_detail(request, slug):
+    """
+    Display an individual :model:`menu.Menu`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`menu.Menu`.
+
+    **Template:**
+
+    :template:`menu/menu_detail.html`
+    """
+
+    queryset = Menu.objects.filter(status=1)
+    menu = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "menu/menu_detail.html",
+        {"menu": menu},
+    )
