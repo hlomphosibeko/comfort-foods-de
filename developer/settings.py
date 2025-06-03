c25a9fed44e4782fbba38ff82d00074ca57712cd
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-06-03 08:24:24 +0000

    Create ordercart app, modify the models, register app

--- developer/settings.py
+++ developer/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'foody',
     'menu',
+    'ordercart',
     # 'reservations', 
 ]
 
@@ -110,6 +112,9 @@ WSGI_APPLICATION = 'developer.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://localhost",
