3cd57be9fdcc09418d9e8deef53379f801a3885b
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-06-04 11:20:02 +0000

    Remove ordercart app, create reservations app

--- developer/settings.py
+++ developer/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'cloudinary',
     'foody',
     'menu',
-    'ordercart',
     # 'reservations', 
 ]
 
--- ordercart/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import OrderCart, Cart
-
-# Register your models here.
-admin.site.register(OrderCart)
-admin.site.register(Cart)
