a83c6542044c901ce9b2b9f422936e4f75d5c47b
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-06-11 13:22:24 +0000

    Creating tests for the menu and reservation views

--- menu/test_views.py
+++ menu/test_views.py
@@ -14,7 +14,7 @@ class TestMenuViews(TestCase):
         )
         self.menu = Menu(meal_title="Menu title", customer=self.user,
                         slug="meal-title", content="Menu content", price=35,
-                        category="Hearty Meals", status=0)
+                        category="Name", status=0)
         self.menu.save()
 
     def test_render_menu_detail_page_with_customer_comment_form(self):
---
+++ reservation/test_views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+from django.urls import reverse
+from django.test import TestCase
+from .models import Reservation
+
+class TestReservationViews(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username="myUsername",
+            password="myPassword"
+        )
+        self.reservation = Reservation(customer=self.user, table_service="Table Service",
+                        day='2025-06-13', time="17:00 PM",
+                        time_ordered='2025-06-13 17:00')
+        self.reservation.save()
+
+    def test_get_table_booking_request(self):
+        response = self.client.get(reverse('tableBooking'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tableBooking.html')
+
+
+class TestBookingSubmitViews(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='myUsername',
+            password='myPassword'
+        )
+        self.client.login(username='myUsername', password='myPassword')
+        self.session = self.client.session
+        self.session["day"] = (datetime.now() + timedelta(days=21)).strftime('%Y-%m-%d')
+        self.session["table_service"] = "Table for 2"
+        self.session.save()
+
+    def test_get_booking_submission_request(self):
+        response = self.client.get(reverse('bookingSubmit'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookingSubmit.html')
